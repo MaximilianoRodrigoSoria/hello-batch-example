bf5cf2a44231057c0f1f6dfd7e58d3e639173a63
Maximiliano Rodrigo Soria <MaximilianoRodrigoSoria@gmail.com>
2024-04-05 10:51:15 -0300

    Se agrega Swagger

--- src/main/java/ar/com/laboratory/hellobatchexample/controllers/JobController.java
+++ src/main/java/ar/com/laboratory/hellobatchexample/controllers/JobController.java
@@ -1,17 +1,37 @@
-//package ar.com.laboratory.hellobatchexample.controller;
-//
-//import io.swagger.v3.oas.annotations.Operation;
-//import io.swagger.v3.oas.annotations.tags.Tag;
-//import org.springframework.batch.core.JobParametersInvalidException;
-//import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
-//import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
-//import org.springframework.batch.core.repository.JobRestartException;
-//import org.springframework.http.ResponseEntity;
-//
-//@Tag(name = "Batch", description = "Operations jobs")
-//public interface JobController {
-//
-//
-//    @Operation(summary = "Execution Job", description = "Execution Job")
-//    public ResponseEntity<String> runJub() throws JobInstanceAlreadyCompleteException, JobExecutionAlreadyRunningException, JobParametersInvalidException, JobRestartException;
-//}
+package ar.com.laboratory.hellobatchexample.controllers;
+
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.AllArgsConstructor;
+import org.springframework.batch.core.*;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/jobs")
+@AllArgsConstructor
+@Tag(name = "Batch", description = "Operations jobs")
+public class JobController {
+
+    private JobLauncher jobLauncher;
+
+    private Job job;
+
+
+    @GetMapping("/run")
+    @Operation(summary = "Execution Job", description = "Execution Job")
+    public String runJub() throws JobInstanceAlreadyCompleteException, JobExecutionAlreadyRunningException, JobParametersInvalidException, JobRestartException {
+        JobParameters jobParameters = new JobParametersBuilder()
+                .addLong("time", System.currentTimeMillis())
+                .toJobParameters();
+        JobExecution execution = jobLauncher.run(job, jobParameters);
+        return "OK";
+    }
+}
--- src/test/java/ar/com/laboratory/hellobatchexample/HelloBatchExampleApplicationTests.java
+++ src/test/java/ar/com/laboratory/hellobatchexample/HelloBatchExampleApplicationTests.java
@@ -3,7 +3,7 @@ package ar.com.laboratory.hellobatchexample;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class HelloBatchExampleApplicationTests {
 
     @Test
